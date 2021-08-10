387950ae861f33d304e80459646b79e4facffdda
Gurunarendra46 <86468947+Gurunarendra46@users.noreply.github.com>
2021-08-10 17:25:38 +0530

    Add files via upload

---
+++ webpage/authentication/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDd2HHFvyI-49ivWsRi2IsNCxYg2-3KZ1o",
+    authDomain: "lyrical-compass-321008.firebaseapp.com",
+    projectId: "lyrical-compass-321008",
+    storageBucket: "lyrical-compass-321008.appspot.com",
+    messagingSenderId: "354420458723",
+    appId: "1:354420458723:web:88ef567c7d9493e640f1d6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
