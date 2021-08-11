9d0df2c7b6409b198d5d5e79dd25e6f6d7cf801f
Gurunarendra46 <86468947+Gurunarendra46@users.noreply.github.com>
2021-08-11 17:48:08 +0530

    Update index.js

--- webpage/authentication/index.js
+++ webpage/authentication/index.js
@@ -18,21 +18,43 @@ function login(){
 }
 
 function signUp(){
+    //const email = document.getElementById("email").value
+    //const password = document.getElementById("password").value
+    //firebase.auth().createUserWithEmailAndPassword(email, password)
+    //.catch((error) => {
+     //   document.getElementById("error").innerHTML = error.message
+    //});
     const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        document.getElementById("error").innerHTML = "link sent to your email id verify and set the password then sign in"
+        alert("link sent to your email id,verify and set the password")
+    })
     .catch((error) => {
         document.getElementById("error").innerHTML = error.message
     });
+
 }
 
 function forgotPass(){
     const email = document.getElementById("email").value
     firebase.auth().sendPasswordResetEmail(email)
     .then(() => {
-        alert("Reset link sent to your email id")
+        document.getElementById("error").innerHTML = "Reset link sent to your email id ,check -verify then sign in"
+        //alert("Reset link sent to your email id")
     })
     .catch((error) => {
         document.getElementById("error").innerHTML = error.message
     });
 }
+
+function emailVerify2(){
+        const email = document.getElementById("email").value
+        firebase.auth().currentUser.sendEmailVerification()
+  .then(() => {
+    // Email verification sent!
+    document.getElementById("error").innerHTML = "verification mail sent to your email id  "
+
+    // ...
+  });
+  }
