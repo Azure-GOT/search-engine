067e08a5fd14dac4b6eabcbceb71b4fad0478ff9
Gurunarendra46 <86468947+Gurunarendra46@users.noreply.github.com>
2021-08-17 16:27:05 +0530

    Update index.js

--- webpage/authentication/index.js
+++ webpage/authentication/index.js
@@ -2,16 +2,63 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
     event.preventDefault()
 })
 
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        location.replace("welcome.html")
-    }
-})
+//firebase.auth().onAuthStateChanged((user)=>{
+  //  if(user){
+       // location.replace("welcome.html")
+   // }
+//})
 
 function login(){
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
     firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+        var user=firebase.auth().currentUser;
+        //alert(user.email)
+        var email_id=user.email;
+        var email_verified=user.emailVerified;
+        if(!email_verified){
+        //alert("email is not verified")
+        //<a href="welcome.html"  style="color:red;">click here to enter the website</a><br><br>
+        document.getElementById('check1').innerHTML = `
+
+          <br><br><center>
+          <p>Sorry To Say</p>
+          <p>Your Account Is Not Verified </p>
+          <p>Verification of  Your Mail is needed To login, check ur email </p>
+          <a href="index.html"  style="color:red;">Home Page</a><br><br></center>`
+
+        //document.getElementById('check1').innerHTML = `<br><br>`
+        //document.getElementById('check1').innerHTML = `
+          //<br><br><center>
+          //<p>Welcome</p>
+          //<p> </p>
+          //<a href="welcome.html"  style="color:red;">click here to enter the website</a><br><br>
+          //<a href="index.html"  style="color:red;">Home Page</a><br><br></center>`
+        //if(email_verified==false){
+        //document.getElementById("check1").innerHTML = "email not verified";
+        //alert("not verified")
+
+        }
+        else{
+                firebase.auth().onAuthStateChanged((user)=>{
+                if(user){
+                            location.replace("welcome.html")
+                        }
+                 })
+        //document.getElementById('check1').innerHTML ="none"
+        //alert("email is not verified")
+        //<a href="welcome.html"  style="color:red;">click here to enter the website</a><br><br>
+        //document.getElementById('check1').innerHTML = `
+         // <br><br><center>
+          //<p>Sorry To Say</p>
+          //<p>Your Account Is Not Verified </p>
+          //<p>Verify Your Mail To login </p>
+          //<a href="index.html"  style="color:red;">Home Page</a><br><br></center>`
+        }
+        //document.getElementById("error").innerHTML = "link sent to your email id verify and set the password then sign in"
+        //alert("link sent to your email id,verify and set the password")
+    })
     .catch((error)=>{
         document.getElementById("error").innerHTML = error.message
     })
@@ -26,6 +73,7 @@ function signUp(){
     //});
     const email = document.getElementById("email").value
     firebase.auth().sendPasswordResetEmail(email)
+
     .then(() => {
         document.getElementById("error").innerHTML = "link sent to your email id verify and set the password then sign in"
         alert("link sent to your email id,verify and set the password")
