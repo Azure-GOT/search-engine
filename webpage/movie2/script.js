086a18e697f0d90a418b81bdd642160c99155975
Gurunarendra46 <86468947+Gurunarendra46@users.noreply.github.com>
2021-08-25 18:24:43 +0530

    Update script.js

--- webpage/movie2/script.js
+++ webpage/movie2/script.js
@@ -41,6 +41,41 @@ jsArrow.onclick = function() {
 
 
 //==================================================================================================
+/*function script3Demo(){
+//TMDB
+
+const API_KEY = 'api_key=9a85a018f878f3af62f324c75c332248';
+const BASE_URL = 'https://api.themoviedb.org/3';
+//use the below api url for the most popular movies
+const API_URL = BASE_URL + '/discover/movie?region=IN&'+API_KEY+'&with_original_language=te';
+//use the below api url for the best movies from 2010
+const API2_URL = BASE_URL + '/discover/movie?primary_release_year=2020&sort_by=vote_average.desc&'+API_KEY;
+//use the below api url for the movies are in theatres
+const API3_URL = BASE_URL + '/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&'+API_KEY;
+
+//use the below api url for the highest rated movies
+
+const API4_URL = BASE_URL + 'the highest rated movies rated R&'+API_KEY;
+//https://api.themoviedb.org/3/movie/550?api_key={api_key}&language=en-US&append_to_response=images&include_image_language=en,null
+//https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=de-DE&region=DE&release_date.gte=2016-11-16&release_date.lte=2016-12-02&with_release_type=2|3
+
+const TAMIL_URL = BASE_URL + '/discover/movie?region=IN&'+API_KEY+'&with_original_language=ta';
+//const HINDI_URL = BASE_URL + '/discover/movie?region=IN&'+API_KEY+'&with_original_language=hi';
+//return TAMIL_URL;
+
+//----------------------------------------------------------testing zone------------------------------------------------------
+const tamil = document.getElementById('tamil');
+//window.alert(tamil);
+//tamil.addEventListener('click', () => {
+var cv=document.getElementById("tamil").addEventListener("click", function mine() {
+  var url1 =TAMIL_URL ;
+  return url1
+});
+//return TAMIL_URL;
+return cv
+}
+*/
+
 
 
 //-------------------------------------------------------------------------------------------------------------
@@ -201,7 +236,43 @@ var el_up = document.getElementById("");
                         //alert(xy);
                         }
                     }
-       
+        /*function demosum(){
+        for (var i = 0; i < z.length; i++) {
+                        if(Object.values(z)[i]=="hindi"){
+                                                    var xyz=HINDI_URL;
+                                                    //document.getElementById(Object.values(z)[i]).addEventListener("click", function() {
+                                                     return getMovies(xyz);
+                                                    //alert("Hello World!"+xy);
+                                                    //});
+                        //alert(Object.values(z)[i]);
+                        //alert(xy);
+                        }
+                    }}
+        for (var i = 0; i < z.length; i++) {
+                        if(Object.values(z)[i]=="search"){
+                                                    //var k=KANNADA_URL;
+
+                                                    form.addEventListener('submit', (e) => {
+                                                        e.preventDefault();
+
+                                                         const searchTerm = search.value;
+
+                                                        if(searchTerm) {
+                                                            getMovies(searchURL+'&query='+searchTerm);
+                                                        }
+                                                        else{
+                                                        getMovies(lastUrl);
+                                                        }
+
+                                                    })
+                                                    //alert("Hello World!"+xy);
+                                                   }
+
+                        //alert(Object.values(z)[i]);
+                        //alert(xy);
+                        }
+                        */
+
 
 
 //return xy;
@@ -268,14 +339,21 @@ function showMovies(data) {
 
     data.forEach(movie => {
         const {title, poster_path, vote_average, overview, id} = movie;
+        var vote1=vote_average
+        if(vote1>0){
+        vote1=vote_average;
+        }
+        else{
+        vote1="TBA";
+        }
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+             <img src="${poster_path? IMG_URL+poster_path: "https://via.placeholder.com/950/000000/FFFFFF/?text=No Poster Available" }" alt="${title}">
 
             <div class="movie-info">
                 <h3>${title}</h3>
-                <span id="sample" class="${getColor(vote_average)} d-flex-row justify-content right">${vote_average}</span>
+                <span id="sample" class="${getColor(vote1)} d-flex-row justify-content right">${vote1}</span>
             </div>
 
 
@@ -309,15 +387,101 @@ function getColor(vote) {
         return 'green'
     }else if(vote >= 5){
         return "orange"
-    }else{
-        return 'red'
+    }else if(vote> 0){
+    return "red"
+    }
+    else{
+        return 'blue'
     }
 }
 
 
 
 //--------------------------------------genre list-------------------------------------------------------
+function gen(){
+const genres =
+   [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
 
+console.log(genres)
+//document.getElementById(genre);
+}
 //---------------------------------------------------||-----------------------------
 //for click it will load page
 //window.onload = function() {
